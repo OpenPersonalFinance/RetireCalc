c6ffd6b0dac61fe416f885528047a6ba9bed67e8
Josh Wittner <jwittner@gmail.com>
2016-01-27 11:34:16 -0800

    Setup the slider to use ng-model for automatic data binding. Got the script in the controller to work.

--- Ionic/Application/www/js/controllers.js
+++ Ionic/Application/www/js/controllers.js
@@ -2,4 +2,8 @@ angular.module('starter.controllers', [])
 
 .controller('CalculateCtrl', function($scope) {
   $scope.income = 45000;
+
+  $scope.outputUpdate = function (val) {
+    this.income=val;
+  }
 });
