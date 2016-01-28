6bdccf0674f6bcaf18d5461e2e271be7ed437652
Josh Wittner <jwittner@gmail.com>
2016-01-27 16:37:12 -0800

    Dropped controller method as ng-model does the full binding!

--- Ionic/Application/www/js/controllers.js
+++ Ionic/Application/www/js/controllers.js
@@ -2,8 +2,4 @@ angular.module('starter.controllers', [])
 
 .controller('CalculateCtrl', function($scope) {
   $scope.income = 45000;
-
-  $scope.outputUpdate = function (val) {
-    this.income=val;
-  }
 });
