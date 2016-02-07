ec11b02cbc07ccf3422f3604aee75ca578aa8b8f
Matt Casanova <lazersquad@gmail.com>
2016-02-08 00:54:23 +0900

    Added Seperate functions to calculate graph and text fields.  Added comments for both functions.  Added first draft of graph functionality.

--- Ionic/Application/www/js/controllers.js
+++ Ionic/Application/www/js/controllers.js
@@ -9,66 +9,139 @@ angular.module('starter.controllers', [])
   var nestEgg = 0;
   var yearsToRetire = 0;
   
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Spending', 'Income'];
-  $scope.data = [
-    [50, 50, 50, 50, 50, 50, 50],
-    [2, 4, 8, 16, 32, 64, 128]
-  ];
-  $scope.colours = [
-    {
-      fillColor: 'rgba(255, 0, 0, .1)',
-      strokeColor: 'Red',
-      
-    }, 
-    {
-      fillColor: 'rgba(0, 255, 0, .4)',
-      strokeColor: 'Green',
-    }
+  
+  $scope.labels = []; //names for x axis on graph
+  $scope.series = []; //Names for each of the colors
+  $scope.data   = []; //the values for each set
+  //colors for each set, used British spelling because that is what the charts use
+  $scope.colours = [{
+    fillColor: 'rgba(255, 0, 0, .1)',
+    strokeColor: 'Red',
+  }, 
+  {
+    fillColor: 'rgba(0, 255, 0, .4)',
+    strokeColor: 'Green',
+  }
   ];
 
-  function calculateRetirement() {
-
-    //get spending and savings as floats instead of money
-    var spending       = $scope.monthlySpending;
-    var savings        = $scope.monthlySavings;
-
-    //the number of months I save/invest
-    var months         = 0;
-    var netWorth       = $scope.currentNetWorth;
-    var earnRate       = $scope.expectedReturn / 100.0;
-    var spendRate      = $scope.withdrawalRate / 100.0;
+  ////////////////////////////////////////////////////////////////////////////
+  //Calculates the graph data given a number of years to retire. 
+  //This function is called in calculate retirement 
+  //assumes either savings and networth data is valid
+  ////////////////////////////////////////////////////////////////////////////
+  function calculateGraph(yearsUntilRetirement){
     
     //clear graph data
-    $scope.labels = [];
-    $scope.data = [];
+    $scope.labels      = [];
+    $scope.data        = [];
     var graphSpendings = [];
-    var graphIncomes = [];
-
-    //based on netWorth and withdrawal rate
-
-    //loop until my investments are more than my spending
-    // NOTE: We should deduce the math to determine months/nestEgg directly
-    if( savings > 0 || netWorth > 0)
-    {
-      while(spending > (netWorth * spendRate) / 12.0)
-      {
-        //add savings and growth rate
-        //growth rate is per month, not year, so 1/12
-        netWorth *= Math.pow(1.0 + earnRate / 12, 1);
-        netWorth += savings;
-
-        ++months;
-      }
-    }
-
-     var totalTest =
-       (currentNetWorth * Math.pow(1 + (earnRate / 12), months))
-       + (savings * ((Math.pow(1+earnRate/12, months) - 1) / (earnRate/12)));
-
-    nestEgg = netWorth;
-    yearsToRetire = (months / 12.0).toFixed(1);
+    var graphIncomes   = [];
+      
+    var MONTHS_PER_YEAR = 12;
+    var yearlySpending  = $scope.monthlySpending * MONTHS_PER_YEAR;
+    var yearlySavings   = $scope.monthlySavings  * MONTHS_PER_YEAR;
+    var yearlyEarnRate  = $scope.expectedReturn / 100.0;
+    var yearlyWithdrawl = $scope.withdrawalRate /100.0;
+    var netWorth        = $scope.currentNetWorth; 
+    var yearlyInterest  = netWorth * yearlyWithdrawl;
+    var steps           = 1;//the number of years between each verical bar in the graph
     
+    //Change value of steps based on number of years to retire
+    //This will make the graph look better
+    //However there are a lot of magic numbers here
+    //What is a good way to deal with this in Javascript???
+    if(yearsUntilRetirement > 50){
+      steps = 7;
+    }
+    else if(yearsUntilRetirement > 30){
+      steps = 5;
+    }
+    else if(yearsUntilRetirement > 15){
+      steps = 3;
+    }
+    else{
+      steps = 1;
+    }
+    
+      
+    
+    //now that we have year, lets calculate graph data
+    for(var i = 0; i < yearsUntilRetirement + (2 * steps); ++i)
+    {
+      netWorth *= 1 + yearlyEarnRate;
+      netWorth += yearlySavings;
+      yearlyInterest = netWorth * yearlyWithdrawl;
+        
+      if(i % steps === 0) {
+        $scope.labels.push("Year " + (i + 1).toString());
+        graphSpendings.push(yearlySpending);
+        graphIncomes.push(yearlyInterest);
+      } 
+    }
+    $scope.data.push(graphSpendings);
+    $scope.data.push(graphIncomes);
+  }
+  ////////////////////////////////////////////////////////////////////////////
+  //Function to populate the text field data
+  //returns number of years to retire which gets passed to calculate graph function
+  //assumes either savings and networth data is valid
+  ////////////////////////////////////////////////////////////////////////////
+  function calculateTextFields(){
+    var MONTHS_PER_YEAR  = 12.0;
+    var totalMonths      = 0;  //the number of months I save/invest
+    var spending         = $scope.monthlySpending;  
+    var savings          = $scope.monthlySavings;
+    var netWorth         = $scope.currentNetWorth;
+    var monthlyEarnRate  = ($scope.expectedReturn / 100.0)/ MONTHS_PER_YEAR;
+    var monthlyWithdrawl = ($scope.withdrawalRate / 100.0)/MONTHS_PER_YEAR;
+    var monthlyInterest  = netWorth * monthlyWithdrawl;
+    
+    
+    // NOTE: We could deduce the math to determine months/nestEgg directly
+    // but our nest egg will always be 25 times annual spending
+      
+      
+    //loop until my investments are more than my spending
+    while(spending > monthlyInterest) {
+      //first add last months interest
+      netWorth *= 1.0 + monthlyEarnRate;
+      //then add this months contributions
+      netWorth += savings;
+      //how much investment income can I now get?
+      monthlyInterest = netWorth * monthlyWithdrawl;
+      //updates months so I know how many years it will take
+      ++totalMonths;
+    }
+      
+    //now I have my total nest egg
+    nestEgg = netWorth;
+    yearsToRetire = (totalMonths / MONTHS_PER_YEAR).toFixed(1);
+    
+    return (totalMonths / MONTHS_PER_YEAR);
+    
+  }
+  ////////////////////////////////////////////////////////////////////////////
+  //Function to calculate text fields and graph data based on slider input
+  ////////////////////////////////////////////////////////////////////////////
+  function calculateRetirement() {
+    if($scope.monthlySavings === 0 && $scope.currentNetWorth === 0){
+      //can't retire at all, what should we do here?
+      nestEgg       = 0;
+      yearsToRetire = 0;
+      $scope.labels = ["Can't Retire!!!"];
+      $scope.data   = [[$scope.yearlySpending], [0]];
+      return;
+    }
+ 
+    var totalYears = calculateTextFields();
+    //Calculate graph based on years to retire
+    calculateGraph(totalYears);
+        
+    
+    // var totalTest =
+    // (currentNetWorth * Math.pow(1 + (earnRate / 12), months))
+    // + (savings * ((Math.pow(1+earnRate/12, months) - 1) / (earnRate/12)));
+
   };
 
   Object.defineProperties($scope, {
