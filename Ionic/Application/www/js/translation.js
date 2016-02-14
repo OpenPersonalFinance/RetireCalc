86d3ef75acc05596124b27eb4deb5ab8db89359e
Matt Casanova <lazersquad@gmail.com>
2016-02-14 15:21:34 +0900

    Changed my tabs to use translations instead of hard coding strings.  Added explanations to Withdrawal page which doesn't use translations yet.

--- Ionic/Application/www/js/translation.js
+++ Ionic/Application/www/js/translation.js
@@ -3,19 +3,27 @@ var Translation = {
     supportedLanguages : ['en'],
     en:
     {
-      calculate_viewTitle: "When Can I Retire?",
-      calculate_monthlyIncome: "Monthly Income",
-      calculate_currentNetWorth: "Net Worth",
-      calculate_savingRate: "Saving Rate",
-      calculate_expectedReturn: "Return Rate",
-      calculate_withdrawalRate: "Withdrawal Rate",
-      calculate_monthlySavings: "Monthly Savings",
-      calculate_monthlySpending: "Monthly Spending",
-      calculate_yearlySpending: "Yearly Spending",
-      calculate_nestEgg: "Nest Egg",
-      calculate_retirementDate: "Retirement Date",
+      calculate_viewTitle:         "When Can I Retire?",
+      calculate_monthlyIncome:     "Monthly Income",
+      calculate_currentNetWorth:   "Net Worth",
+      calculate_savingRate:        "Saving Rate",
+      calculate_monthlySavings:    "Monthly Savings",
+      calculate_monthlySpending:   "Monthly Spending",
+      calculate_yearlySpending:    "Yearly Spending",
+      calculate_nestEgg:           "Nest Egg",
+      calculate_retirementDate:    "Retirement Date",
       calculate_yearsToRetirement: "Years To Retirement",
-      calculate_chartTitle: "Yearly Spending vs Investing Income"
+      calculate_chartTitle:        "Yearly Spending vs Investing Income",
+      return_viewTitle:            "Return Rate",
+      return_returnSlider:         "Return",
+      withdrawal_viewTitle:        "Withdrawal Rate",
+      withdrawal_withdrawalSlider: "Withdrawal",
+      info_viewTitle:              "Information",
+      info_assumptionsTitle:       "Assumptions",
+      info_aboutTitle:             "About",
+      about_viewTitle:             "Open Personal Finance",
+      details_viewTitle:           "Details"
+      
     },
 
     registerWithProvider: function(provider){
