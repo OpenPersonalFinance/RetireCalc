4819e0eac77dc33ff2efa8f438f4e10c79279dfa
Matt Casanova <lazersquad@gmail.com>
2016-03-13 13:16:49 +0900

    Added translation to return tab

--- Ionic/Application/www/js/translation.js
+++ Ionic/Application/www/js/translation.js
@@ -31,7 +31,12 @@ var Translation = {
       withdrawal_p6:               "$40,000 / 5% = $800,000",
       withdrawal_p7:               "Based on the Trinity Study, a withdrawal rate of 4% gives a retiree a 96% chance of having enough money for a 30 year retirement.  In fact in many of the experiments the retiree not only had enough money for 30 years, the money actually grew such that that they had enough money a much longer retirement.",
       withdrawal_p8:               "Even though some people have raised concerns about the assumtions that went into the Trinity Study, a withdrawal rate of 4% is still used as a rule of thumb for many finacial advisors and is often called the '4% rule' or the '4% Safe Withdrawal Rate'.",
-      withdrawal_p9:               "If you are curious about the conerns or assumptions, we encourage you to reseach on your own."
+      withdrawal_p9:               "If you are curious about the conerns or assumptions, we encourage you to reseach on your own.",
+      
+      return_p1:                   "Over the last 100 years the average return of the stock market has been around 11%, before expenses such as taxes, investment fees and inflation.  This is known as the nominal return. After you have adjusted for these expenses you are left with your real return.",
+      return_p2:                   "On average inflation has been around 3% per year.  So you can do a quick calculation by subtracting inflation from the nominal return to get an expected return of 7%.  This isn't the actual formula to account for inflation, but it is close enough for our purpose.",
+      return_p3:                   "There is no guarentee that the next 100 years will be anything like the last 100. Past performance does not predict future results.  However, this is the best information available.",
+      return_p4:                   "We have chosen a conservative return 5%.  Feel free to be more or less conservative in your estimate."
       
       
     },
